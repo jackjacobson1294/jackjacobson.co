954778bcb5ceb4f431027092c4b669a097151b89
jackstro <jackstro@umich.edu>
2016-11-25 17:36:49 -0500

    November 25 5:36pm

--- app.py
+++ app.py
@@ -26,3 +26,7 @@ def render_Music():
 @app.route('/test_route')
 def test():
     return 'WE GOT ROUTES!'
+
+if __name__ == "__main__":
+	port = int(os.environ.get("PORT", 5000))
+	app.run(host='0.0.0.0', port=port)
